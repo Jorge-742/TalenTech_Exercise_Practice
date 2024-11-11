a0fb40387a7ac4208cf9123186d1775aea9c730c
Jorge-742 <119258841+Jorge-742@users.noreply.github.com>
2024-11-10 22:27:53 -0500

    Crea la tabla empleados e inserta información en sus campos

--- Laboratorio_5_comandos.sql
+++ Laboratorio_5_comandos.sql
@@ -3,3 +3,18 @@
 CREATE DATABASE empresa;
 --- Llama o trae la base de datos.
 USE empresa;
+----------------------------------------------------------------
+--- Crea la tabla llamada empleados con sus respectivos campos.
+CREATE TABLE empleados (
+	id_empleado INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(225) NOT NULL,
+    cargo VARCHAR (50) NOT NULL,
+    salario DECIMAL);
+
+--- Inserta datos en los campos de la tabla empleados.
+INSERT INTO empleados (nombre, cargo, salario)
+	VALUES
+    	('Juan Pérez', 'Gerente', 7500000),
+        ('Ana Gómez', 'Desarrollador', 6000000),
+        ('Luis Castro', 'Proyectos', 10000000);
+----------------------------------------------------------------
