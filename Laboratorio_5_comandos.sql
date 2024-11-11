ddc814a0b9d313a4c96b120b0ae8ea501dcf0992
Jorge-742 <119258841+Jorge-742@users.noreply.github.com>
2024-11-10 22:39:15 -0500

    Eliminación de un libro de la base de datos

--- Laboratorio_5_comandos.sql
+++ Laboratorio_5_comandos.sql
@@ -77,3 +77,7 @@ WHERE id_libro = 1;
 UPDATE prestamos
 SET fecha_devolucion = '2024-08-30'
 WHERE id_prestamo = 1;
+
+--- Eliminación de un libro de la base de datos
+DELETE FROM libros
+WHERE id_libro = 3;
