2c3634fb76779a9aa8562b05c819e20537e820f7
Jorge-742 <119258841+Jorge-742@users.noreply.github.com>
2024-11-10 22:28:45 -0500

    Crea la tabla de cargos e inserta información en sus campos

--- Laboratorio_5_comandos.sql
+++ Laboratorio_5_comandos.sql
@@ -18,3 +18,16 @@ INSERT INTO empleados (nombre, cargo, salario)
         ('Ana Gómez', 'Desarrollador', 6000000),
         ('Luis Castro', 'Proyectos', 10000000);
 ----------------------------------------------------------------
+--- Crea la tabla llamada cargos con sus respectivos campos.
+CREATE TABLE cargos (
+	id_cargo INT PRIMARY KEY AUTO_INCREMENT,
+	nombre_cargo VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(1000),
+	salario DECIMAL);
+--- Inserta datos en los campos 'nombre_cargo' y 'salario' de la tabla cargos.
+INSERT INTO cargos (nombre_cargo, salario)
+	VALUES
+    	('Gerente', 500000),
+        ('Asistente', 250000),
+        ('Desarrollador', 400000);
+-----------------------------------------------------------------
